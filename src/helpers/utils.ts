fe2ec76273f263514bb2f5e7d387e6b42fbbe3fb
Kseniia <feirs911@gmail.com>
2025-02-11 16:38:51 +0300

    fix localstorage

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -8,5 +8,5 @@ export const debounce = (fn: (...args: any) => void, ms = 1000) => {
   };
 };
 export const saveToLocalStorage = (key: string, value: string) => localStorage.setItem(key, JSON.stringify(value));
-export const getFromLocalStorage = (key: string) => JSON.parse(localStorage.getItem(key) || '');
+export const getFromLocalStorage = (key: string) => JSON.parse(localStorage.getItem(key) || '{}');
 export const saveToLocalStorageWithDebounce = debounce(saveToLocalStorage, 500);
